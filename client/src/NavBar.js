bd419d0cbe020b2714b4661c6ede7c8476739f18
samanthamarberger <slatham04@gmail.com>
2023-10-20 17:16:31 -0600

    added logout to sessions

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./context/user";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
+    const{ logout, loggedIn } = useContext(UserContext)
+
+    
     return (
         <header>
             <NavLink to='/login'>
